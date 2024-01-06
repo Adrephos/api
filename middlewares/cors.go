91d56960c1aed21062f29fb11ec031eb23f34cde
Adrephos <adrephos@gmail.com>
2024-01-06 18:14:03 -0500

    feat: change cors to allow all origins

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -1,21 +1,10 @@
 package middlewares
 
 import (
-	"time"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func CORSMiddleware() gin.HandlerFunc {
-	return cors.New(cors.Config{
-    AllowOrigins:     []string{"https://foo.com"},
-    AllowMethods:     []string{"PUT", "PATCH"},
-    AllowHeaders:     []string{"Origin"},
-    ExposeHeaders:    []string{"Content-Length"},
-    AllowCredentials: true,
-    AllowOriginFunc: func(origin string) bool {
-      return origin == "https://github.com"
-    },
-    MaxAge: 12 * time.Hour,
-  })
+	return cors.Default()
 }
